f37896361cf843057e04bacd1ea1776c9bc02493
nobledeveloper01 <nobleconcepts22@gmail.com>
2023-08-16 23:52:00 +0100

    first commit

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className='container'>
+        <h1 className='title'>
+            This is the Home Page
+        </h1>
+    </div>
+  )
+}
